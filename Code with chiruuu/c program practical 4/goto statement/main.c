9f1f85a508909db1d959105efc61446b2929baa3
Sugar dadyyy!!! <CHIRAGBHAKUNI07@GMAIL.COM>
2025-08-13 15:17:38 +0530

    Add files via upload

---
+++ Code with chiruuu/c program practical 4/goto statement/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ int num;
+
+    printf("enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        goto error;
+    printf("you entered a valid number: %d\n", num);
+    return 0;
+    error:
+        printf("error: negative umber not allowed.\n");
+    return 1;
+}
